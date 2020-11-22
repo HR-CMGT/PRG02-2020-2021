52af1ef3ed9657fda2d9b9c5c710b1e0e884a9f3
antwan <antwanvdm@gmail.com>
2020-11-22 15:34:09 +0100

    Results homework week 1

---
+++ week1/finished/opdracht1-4/index.php
@@ -0,0 +1,106 @@
+<?php
+//Fill in your own birthday
+$birthday = "0724";
+
+$constellations = [
+    [
+        "startDate" => "0121",
+        "endDate" => "0218",
+        "name" => "Waterman"
+    ],
+    [
+        "startDate" => "0219",
+        "endDate" => "0320",
+        "name" => "Vissen"
+    ],
+    [
+        "startDate" => "0321",
+        "endDate" => "0420",
+        "name" => "Ram"
+    ],
+    [
+        "startDate" => "0421",
+        "endDate" => "0521",
+        "name" => "Stier"
+    ],
+    [
+        "startDate" => "0522",
+        "endDate" => "0621",
+        "name" => "Tweeling"
+    ],
+    [
+        "startDate" => "0622",
+        "endDate" => "0723",
+        "name" => "Kreeft"
+    ],
+    [
+        "startDate" => "0724",
+        "endDate" => "0823",
+        "name" => "Leeuw"
+    ],
+    [
+        "startDate" => "0824",
+        "endDate" => "0923",
+        "name" => "Maagd"
+    ],
+    [
+        "startDate" => "0924",
+        "endDate" => "1023",
+        "name" => "Weegschaal"
+    ],
+    [
+        "startDate" => "1024",
+        "endDate" => "1122",
+        "name" => "Schorpioen"
+    ],
+    [
+        "startDate" => "1123",
+        "endDate" => "1222",
+        "name" => "Boogschutter"
+    ]
+//    ,
+//    [
+//        "startDate" => "1223",
+//        "endDate" => "0120",
+//        "name" => "Steenbok"
+//    ]
+];
+
+/**
+ * Retrieve constellation for given birthday
+ *
+ * @param $birthday
+ * @param $constellations
+ * @return string
+ */
+function getConstellationForBirthday($birthday, $constellations)
+{
+    //Default constellation is Steenbok all other will be checked in foreach
+    $result = 'Steenbok';
+
+    foreach ($constellations as $constellation) {
+        if ($birthday >= $constellation['startDate'] && $birthday <= $constellation['endDate']) {
+            $result = $constellation['name'];
+        }
+    }
+
+    return $result;
+}
+
+//Call the function to retrieve your own constellation
+$myConstellation = getConstellationForBirthday($birthday, $constellations);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Mijn Sterrenbeeld</title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<h1>Opdracht 1.4 - Sterrenbeeld</h1>
+<hr/>
+<p>
+    Mijn sterrenbeeld is: <?= $myConstellation; ?>
+</p>
+</body>
+</html>
